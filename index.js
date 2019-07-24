46b79f5e1c517ceef048f2e84f14b9d4dc2047c6
Braden Mason <bradenemason@gmail.com>
2019-07-23 22:52:22 -0500

    create, read, and delete are working

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const db = require("./data/hubs-model.js");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (request, response) => {
+  response.send("hello world from express!");
+});
+
+var now = new Date().toISOString();
+
+server.get("/now", (request, response) => {
+  response.send(now);
+});
+
+server.get("/hubs", (request, response) => {
+  db.find()
+    .then(hubs => {
+      response.status(200).json(hubs);
+    })
+    .catch(err => {
+      response.status(500).json({ success: false, err });
+    });
+});
+
+server.post("/hubs", (request, response) => {
+  const hubInfo = request.body;
+  console.log(hubInfo);
+
+  db.add(hubInfo)
+    .then(hub => {
+      response.status(201).json({ success: true, hub });
+    })
+    .catch(err => {
+      response.status(500).json({ success: false, err });
+    });
+});
+
+server.delete("/hubs/:id", (request, response) => {
+  const { id } = request.params;
+
+  db.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        response.status(204).end();
+      } else {
+        response.statue(404).json({
+          success: false,
+          message: "I cannot find the hub you are looking for"
+        });
+      }
+    })
+    .catch(err => {
+      response.status(500).json({ success: false, err });
+    });
+});
+server.listen(4000, () => {
+  console.log("server listening on port 4000");
+});
